d1116eb20ce0db9fd8a7212142a401165625c24c
rustam9724 <rustam_9724@mail.ru>
2022-11-09 10:59:02 +0500

    init: repo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ function Header() {
     return <div>
         <nav className="#9c27b0 purple">
             <div className="nav-wrapper">
-                <a href="!#" className="brand-logo">React Shop</a>
+                <a href="./" className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="https://github.com/Rustam9724/movies-project">Repo</a></li>
+                    <li><a href="https://github.com/Rustam9724/react-shop-projects">Repo</a></li>
                 </ul>   
             </div>
         </nav>
